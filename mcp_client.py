b276f89e828f8d90b31d77a4e177f23e8f3e1adb
Enzo Panettieri <genzop@gmail.com>
2025-12-10 17:42:46 -0300

    Add prompts functions to mcp client

--- mcp_client.py
+++ mcp_client.py
@@ -56,12 +56,14 @@ class MCPClient:
         return await self.session().call_tool(tool_name, tool_input)
 
     async def list_prompts(self) -> list[types.Prompt]:
-        # TODO: Return a list of prompts defined by the MCP server
-        return []
+        # Obtener la lista de prompts desde el servidor MCP utilizando la sesion actual
+        result = await self.session().list_prompts()        
+        return result.prompts
 
     async def get_prompt(self, prompt_name, args: dict[str, str]):
-        # TODO: Get a particular prompt defined by the MCP server
-        return []
+        # Obtener un prompt especifico en el servidor MCP utilizando la sesion actual
+        result = await self.session().get_prompt(prompt_name, args)
+        return result.messages
 
     async def read_resource(self, uri: str) -> Any:
         # Consultar un recurso especifico en el servidor MCP utilizando la sesion actual
