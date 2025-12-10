c01eb6eb7785f2a319a0f3234c35e45c9278c58f
Enzo Panettieri <genzop@gmail.com>
2025-12-10 13:43:03 -0300

    Implement MCP client functions

--- mcp_client.py
+++ mcp_client.py
@@ -42,14 +42,16 @@ class MCPClient:
         return self._session
 
     async def list_tools(self) -> list[types.Tool]:
-        # TODO: Return a list of tools defined by the MCP server
-        return []
+        # Obtener la lista de tools desde el servidor MCP utilizando la sesion actual
+        result = await self.session().list_tools()
+        # Devolver la lista de tools obtenida
+        return result.tools
 
     async def call_tool(
         self, tool_name: str, tool_input: dict
     ) -> types.CallToolResult | None:
-        # TODO: Call a particular tool and return the result
-        return None
+        # Llamar a un tool especifico en el servidor MCP utilizando la sesion actual
+        return await self.session().call_tool(tool_name, tool_input)
 
     async def list_prompts(self) -> list[types.Prompt]:
         # TODO: Return a list of prompts defined by the MCP server
@@ -82,7 +84,9 @@ async def main():
         command="uv",
         args=["run", "mcp_server.py"],
     ) as _client:
-        pass
+        # Consultar lista de tools disponibles en el servidor MCP
+        result = await _client.list_tools()
+        print(result)
 
 
 if __name__ == "__main__":
